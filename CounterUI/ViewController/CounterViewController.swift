617fa2992ee52d14462aed1f59e60d58c8a7ef93
Gospodi <gospodijazzet@gmail.com>
2022-07-13 17:34:43 +0300

    Добавлена функция для обновления label

---
+++ CounterUI/ViewController/CounterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CounterUI
+//
+//  Created by Gospodi on 13.07.2022.
+//
+
+import UIKit
+
+
+class CounterViewController: UIViewController {
+    var count = 0
+    
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    func updateLabels() {
+        counterLabel.text = String(count)
+    }
+    
+    @IBAction func counterButtonPush(_ sender: Any) {
+        updateLabels()
+     }
+}
+
